086914a175f76fac3ad70b04d8d6e93c7a33cb73
solojavier <1.27201@gmail.com>
2013-02-13 15:05:05 -0600

    Adding initial tests

---
+++ spec/app_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe 'mind sweeper' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  let(:thing)            { double('thing') }
+  let(:options)          { { description: 'a thing in my mind' } }
+  let(:expected_options) { options.merge({reviewed: false}) } 
+
+  it 'takes a thing out of my head into the system' do
+    Thing.should_receive(:create!).with(expected_options)
+    
+    post '/things', options 
+    
+    last_response.status.should == 201
+  end
+  
+  it 'shows first thing collected' do
+   Thing.stub(:where).and_return(['thing', 'other_thing'])
+
+   get '/review'
+   
+   last_response.body.should == 'thing'.to_json
+  end
+
+  it 'sweeps a collected thing' do
+    Thing.stub(:find).with('id').and_return(thing)
+    thing.should_receive(:reviewed=).with(true)
+    thing.should_receive(:save).and_return(true)
+
+    post '/things/id/sweep'
+
+    last_response.status.should == 204
+  end
+  
+  it 'fails when trying to sweeps something wrongly' do
+    Thing.stub(:find).with('id').and_return(thing)
+    thing.stub(:reviewed=).with(true)
+    thing.stub(:save).and_return(false)
+
+    post '/things/id/sweep'
+
+    last_response.status.should == 422
+  end
+
+  it 'shows nothing if no items to review' do
+    Thing.stub(:where).and_return([nil])
+    
+    get '/review'
+    
+    last_response.status.should == 404
+  end
+
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require_relative '../app.rb'
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
