d89ab0cf7445c999c9bee738de3eb2da8996d4c1
solojavier <1.27201@gmail.com>
2013-02-13 13:15:57 -0600

    Adding initial logic

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'mongoid'
+require_relative 'models/thing'
 
-get '/' do
-  "mindcollector is live"
+Mongoid.load!("config/mongoid.yml")
+
+get '/review' do
+  thing = Thing.where(reviewed: false).first
+
+  thing ? thing.to_json : 404
+end
+
+post '/things' do
+  options = { description: params[:description], reviewed: false }
+  thing   = Thing.create!(options)
+
+  201
+end
+
+post '/things/:id/sweep' do
+  thing = Thing.find(params[:id])
+  thing.reviewed = true
+  thing.save ? 204 : 422
 end
