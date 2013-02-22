122739d210bfdf05ad991647b7a89335d40658fa
Javier Cervantes <1.27201@gmail.com>
2013-02-22 04:25:22 -0600

    making it ready for production without redirects

--- app.rb
+++ app.rb
@@ -8,18 +8,18 @@ Mongoid.load!("config/mongoid.yml")
 get '/review' do
   idea = Idea.where(reviewed: false).first
 
-  idea ? redirect '/app.html' : 404
+  idea ? 201 : 404
 end
 
 post '/ideas' do
   options = { description: params[:description], reviewed: false }
   idea   = Idea.create!(options)
 
-  redirect '/app.html'
+  idea.to_json
 end
 
 post '/ideas/sweep' do
   idea = Idea.find(params[:id])
   idea.reviewed = true
-  idea.save ? redirect '/app.html' : 422
+  idea.save ? 204 : 422
 end
