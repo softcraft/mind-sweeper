64a206c40eeb05a8f106ec5c74fdef182b39131b
Javier Cervantes <1.27201@gmail.com>
2013-03-30 13:16:40 -0600

    Adding settings

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
-source :rubygems
+source 'http://rubygems.org'
 ruby "1.9.3"
 
 gem 'sinatra'
 gem 'thin'
 gem 'mongoid'
 gem 'roar'
+gem 'sinatra-contrib'
 
 group :test do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
+require 'sinatra/config_file'
 require 'mongoid'
 
 require_relative 'models/idea'
+require_relative 'models/user'
 require_relative 'representers/root'
 
+config_file 'config/config.yml'
 Mongoid.load!("config/mongoid.yml")
 
 get '/' do
